4296a7b35f860ae31d7d4ed8e1acc375bfce6a7a
HarithF <alharith.farhad@gmail.com>
2024-03-11 23:47:39 +0100

    position tracking added

---
+++ src/loc.h
@@ -0,0 +1,44 @@
+#ifndef PROG_LOC_H
+#define PROG_LOC_H
+
+#include <ostream>
+
+namespace H {
+
+extern int num_errors;
+
+struct Pos {
+    Pos() = default;
+    Pos(int row, int col)
+        : row(row)
+        , col(col)
+    {}
+
+    int row = -1;
+    int col = -1;
+};
+
+struct Loc {
+    Loc() = default;
+    Loc(const char* file, Pos begin, Pos finish)
+        : file(file)
+        , begin(begin)
+        , finish(finish)
+    {}
+    Loc(const char* file, Pos pos)
+        : Loc(file, pos, pos)
+    {}
+
+    const char* file = nullptr;
+    Pos begin;
+    Pos finish;
+
+    std::ostream& err() const;
+};
+
+std::ostream& operator<<(std::ostream&, const Pos&);
+std::ostream& operator<<(std::ostream&, const Loc&);
+
+}
+
+#endif
