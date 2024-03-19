661a01fb8095fe0435451084bb082c279edba054
HarithF <alharith.farhad@gmail.com>
2024-03-20 00:04:50 +0100

    sema analysis expanded

--- src/ast.cpp
+++ src/ast.cpp
@@ -206,6 +206,153 @@ void LabeledStmt::check(Sema &sema) {UNUSED(sema);}
 void ErrStmt::check(Sema &sema) {UNUSED(sema);}
 
 
+//! =================================================
+//! ============= Infix Expressions =================
+//! =================================================
+
+Type* InfixExp::check(Sema &sema) {
+    Type* lhs_type = lhs()->check(sema);
+    // std::cout << "LHS: " << lhs_type->str() << std::endl;
+    Type* rhs_type = rhs()->check(sema);
+    // std::cout << "RHS: " << rhs_type->str() << std::endl;
+
+
+    if (lhs_type->str() == "error" || rhs_type->str() =="error") return type_ = new ErrorType();
+
+    // Logical Operators
+    if (operation().isa(Tok::Tag::P_Less) || 
+        operation().isa(Tok::Tag::P_Less_Equal) || 
+        operation().isa(Tok::Tag::P_Greater) || 
+        operation().isa(Tok::Tag::P_Greater_Equal) ||
+        operation().isa(Tok::Tag::P_Equal) ||
+        operation().isa(Tok::Tag::P_Unequal)) {
+            if ((dynamic_cast<ArithmeticType*>(lhs_type) && dynamic_cast<ArithmeticType*>(rhs_type)) || lhs_type->str()==rhs_type->str()) {
+                return type_ = new IntType();
+        }    
+    }
+
+    // Logical And/Or
+    if (operation().isa(Tok::Tag::P_Logical_And) || operation().isa(Tok::Tag::P_Logical_Or)) {
+        if (lhs_type->isScalar() && rhs_type->isScalar()) return type_ = new IntType();
+    }
+    
+    //Assignment
+    if (operation().isa(Tok::Tag::P_Assign)) {
+        
+        if (!dynamic_cast<Identifier*>(lhs()) && !dynamic_cast<PrefixExp*>(lhs()) && !dynamic_cast<ArrayExp*>(lhs()) && !dynamic_cast<MemberAccessExp*>(lhs())){
+            operation().loc().err() << "LHS of assignment must be a modifiable lvalue!" << loc().endErr();
+        } else if (lhs_type->str() != rhs_type->str()) {
+            operation().loc().err() << "Incompatible Types for operand '" << operation().str() << "' (" << lhs_type->str() << "<->" << rhs_type->str() <<")!" << loc().endErr();
+        }
+        return type_ = lhs_type;
+    }
+
+    // Addition
+    if (operation().isa(Tok::Tag::P_Addition)){
+        if (dynamic_cast<ArithmeticType*>(lhs_type) && dynamic_cast<ArithmeticType*>(rhs_type)) return type_ = new IntType();
+        else if (dynamic_cast<PointerType*>(lhs_type) && dynamic_cast<PointerType*>(lhs_type)->pointee()->isComplete() && dynamic_cast<const ArithmeticType*>(rhs_type)) return type_ = lhs_type;
+        else if (dynamic_cast<PointerType*>(rhs_type) && dynamic_cast<PointerType*>(rhs_type)->pointee()->isComplete() && dynamic_cast<const ArithmeticType*>(lhs_type)) return type_ = rhs_type;
+
+    }
+
+    //Substraction
+    if (operation().isa(Tok::Tag::P_Substraction)){
+        if (dynamic_cast<ArithmeticType*>(lhs_type) && dynamic_cast<ArithmeticType*>(rhs_type)) return type_ = new IntType();
+        else if (dynamic_cast<PointerType*>(lhs_type) && dynamic_cast<PointerType*>(lhs_type)->pointee()->isComplete() && dynamic_cast<PointerType*>(rhs_type) && dynamic_cast<PointerType*>(rhs_type)->pointee()->isComplete() && lhs_type->str()==rhs_type->str()) return type_ = new IntType();
+        else if (dynamic_cast<PointerType*>(lhs_type) && dynamic_cast<PointerType*>(lhs_type)->pointee()->isComplete() && dynamic_cast<const IntType*>(rhs_type)) return type_ = lhs_type;
+    }
+
+    // Multiplication and Division
+    if (operation().isa(Tok::Tag::P_Multiplication) || operation().isa(Tok::Tag::P_Division)){
+        if (dynamic_cast<ArithmeticType*>(lhs_type) && dynamic_cast<ArithmeticType*>(rhs_type)) return type_ = new IntType(); 
+    }
+
+    operation().loc().err() << "Incompatible Types for operand '" << operation().str() << "' (" << lhs_type->str() << "<->" << rhs_type->str() <<")!" << loc().endErr();
+    return type_ = sema.error_type();
+}
+
+Type* TernaryExp::check(Sema &sema) {
+    //auto conditionType = condition()->check(sema);
+    auto consequenceType = consequence()->check(sema);
+    auto alternativeType = alternative()->check(sema);
+
+    if (consequenceType->str() != alternativeType->str()) {
+        loc().err() << "Mismatch of type in consequence and alternative of ternary expression (" << consequenceType->str() << "<->" << alternativeType->str() << ")" << loc().endErr();
+        return new ErrorType();
+    }
+    return consequenceType;
+}
+
+Type* PrefixExp::check(Sema &sema) {
+    
+    auto opType = operand()->check(sema);
+
+    if (prefix().isa(Tok::Tag::P_Multiplication)) { 
+        if (dynamic_cast<PointerType*>(opType)) {
+            auto operandType = dynamic_cast<PointerType*>(opType);
+            return type_ = operandType->pointee();
+        } else {
+            loc().err() << "Invalid type argument of unary '*' (have " << opType->str() << ")" << loc().endErr();
+            return new ErrorType();
+        }
+    }
+    if (prefix().isa(Tok::Tag::P_Bitwise_And)) { 
+        return type_ = new PointerType(opType);
+    }
+    if (prefix().isa(Tok::Tag::P_Addition) || prefix().isa(Tok::Tag::P_Substraction)){
+        if (dynamic_cast<ArithmeticType*>(opType)) return type_ = opType;
+        else {
+            loc().err() << "Invalid type argument of unary '"<< prefix() <<"' (have " << opType->str() << ")" << loc().endErr();
+            return new ErrorType();
+        }
+    }
+    if (prefix().isa(Tok::Tag::P_Logical_Not)){
+        if (opType->isScalar()) return type_ = new IntType();
+        else {
+            loc().err() << "Invalid type argument of unary '"<< prefix() <<"' (have " << opType->str() << ")" << loc().endErr();
+            return new ErrorType();
+        }
+    }
+    if (prefix().isa(Tok::Tag::P_Bitwise_Not)){
+        if (dynamic_cast<IntType*>(opType)) return type_ = opType;
+        else {
+            loc().err() << "Invalid type argument of unary '"<< prefix() <<"' (have " << opType->str() << ")" << loc().endErr();
+            return new ErrorType();
+        }
+    }
+
+    return type_ = opType;
+}
+
+Type* MemberAccessExp::check(Sema &sema) {
+    
+    // TODO: Check that member_name is actually part of the object
+    auto objectType = object()->check(sema);
+
+    if (dynamic_cast<ErrorType*>(objectType)) return type_ = new ErrorType();
+
+    // Correct operation for correct type
+    if (dynamic_cast<PointerType*>(objectType) && operation()==Tok::Tag::P_Dot){
+        loc().err() << "Object with pointer type must use '->' operation to access members!" << loc().endErr();
+    } else if (!dynamic_cast<PointerType*>(objectType) && operation()==Tok::Tag::P_Arrow_R) {
+        loc().err() << "Object with object type must use '.' operation to access members!" << loc().endErr();
+    }
+
+    if (dynamic_cast<Identifier*>(object())) {
+        auto obj = dynamic_cast<Identifier*>(object());
+        StructSpecifier* structSpecif = dynamic_cast<StructSpecifier*>(sema.lookup(obj->name())->specifier());
+        std::string structIdent = structSpecif->structIdentifierString();
+        SpecifierDeclarator* member = sema.lookupMember(structIdent, member_name());
+
+        if (!sema.structDefined(structIdent)) loc().err() << "'" <<  obj->name() << "' is not a struct!" << loc().endErr();
+        else if (member == nullptr) loc().err() << "'struct " <<  structIdent << "' has no member named '" << member_name() << "'!" << loc().endErr();
+        else return type_ = member->type();
+    }
+        
+    return type_ = new ErrorType();
+}
+
+
 //! ========================================================================================================
 //! ================= Stream/Dump ==========================================================================
 //! ========================================================================================================
